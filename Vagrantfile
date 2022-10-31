cdddf4bb4e9f43d09c2e2baf2c38b749086b4c66
tvogel <tvogel@student.42.fr>
2022-10-31 16:24:23 +0100

    feat: add a vagrant provision file to install docker

--- Vagrantfile
+++ Vagrantfile
@@ -1,3 +1,5 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "debian/bullseye64"
+  config.vm.provision :shell, path: "vagrant_provision.sh"
+  config.vm.synced_folder ".", "/Inception", create: true
 end
---
+++ vagrant_provision.sh
@@ -0,0 +1,27 @@
+echo "============= APT UPDATE ============="
+
+apt-get update
+apt-get install -y \
+	ca-certificates \
+	curl \
+	gnupg \
+	lsb-release
+
+# Add docker repo to apt
+echo "============= ADD DOCKER REPO TO APT ============="
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+
+# Install Docker
+echo "============= INSTALL DOCKER PACKAGES ============="
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# Upgrade all packages
+echo "============= APT UPGRADE ============="
+apt upgrade -y
