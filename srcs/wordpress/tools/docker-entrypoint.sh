462473b90a51fdeeb373f2f2a9412612d82bf85f
Théo Vogel <contact@theovogel.fr>
2022-12-01 03:52:05 +0100

    fix: no more email error in wp container

--- srcs/wordpress/tools/docker-entrypoint.sh
+++ srcs/wordpress/tools/docker-entrypoint.sh
@@ -9,7 +9,7 @@ else
 	wp db create
 	wp core install --title=wordpress --admin_user=$WP_ADMIN_NAME --admin_password=$WP_ADMIN_PASSWORD --skip-email --admin_email=tvogel@student.42.fr --url=https://tvogel.42.fr --path=/var/www/html
 
-	wp user update $WP_ADMIN_NAME --user_pass=$WP_ADMIN_PASSWORD
+	wp user update $WP_ADMIN_NAME --user_pass=$WP_ADMIN_PASSWORD --skip-email
 	wp user create $WP_USER $WP_USER@42.fr --user_pass=$WP_USER_PASSWORD --role=author --porcelain
 fi
 chmod -R 755 /var/www/html
